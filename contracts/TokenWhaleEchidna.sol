ab655e0bf95c0cd9b4dfe7f32907a39ad88871d7
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-03-09 20:58:38 +0100

    initial setup for fuzzing tokenWhale

---
+++ contracts/TokenWhaleEchidna.sol
@@ -0,0 +1,19 @@
+// SPDX-License Identifier:MIT
+
+pragma solidity ^0.6.0;
+
+import "./TokenWhale.sol";
+
+contract TokenWhaleEchidna {
+    address player;
+    TokenWhaleChallenge token;
+
+    constructor() public {
+        player = msg.sender;
+        token = new TokenWhaleChallenge(player);
+    }
+
+    function echidna_test_balance() public view returns (bool) {
+        return token.balanceOf(player) <= 1000000;
+    }
+}
